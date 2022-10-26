35073db267a10fd7951d293353c37b9d4bf86281
esoe <igor_nikulin@mail.ru>
2022-10-26 18:57:13 +0300

    second

---
+++ src/main/java/ru/molokoin/App.java
@@ -0,0 +1,8 @@
+package ru.molokoin;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Запущен поток: " + Thread.currentThread().getName());
+        System.out.println("second");
+    }
+}
