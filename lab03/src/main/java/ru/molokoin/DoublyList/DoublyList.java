3392a65a9c773df9fbc0463e806774f9dcdba31a
esoe <igor_nikulin@mail.ru>
2022-10-30 12:17:16 +0300

    DoublyList - minimals ready to use

--- lab03/src/main/java/ru/molokoin/DoublyList/DoublyList.java
+++ lab03/src/main/java/ru/molokoin/DoublyList/DoublyList.java
@@ -1,5 +1,8 @@
 package ru.molokoin.DoublyList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class DoublyList {
     private Node head = null;
     private Node tail = null;
@@ -59,11 +62,18 @@ public class DoublyList {
      * вывод в консоль всех элементов списка, по порядку
      * метод скопирован с односвязных списков
      */
-    public void printAll(){
+    public void printForvard(){
         Node tmp = getHead();
         while (tmp != null){
             printNode(tmp);
             tmp = tmp.forvard;
+        }
+    }
+    public void printBackvard(){
+        Node tmp = getTail();
+        while(tmp != null){
+            printNode(tmp);
+            tmp = tmp.backvard;
         }
     }
     /**
@@ -144,29 +154,89 @@ public class DoublyList {
         }
         return isContain;
     }
+    /**
+     * удаляем ноды, содержащие данные совпадающие с указанным в методе значением
+     * @param value
+     */
     public void deleteByValue(Integer value){
+        //проверяем наличие указанного значения в списке
         if (isContain(value)){
+            //удаляем первые ноды, если они содержат указанное значение
             Node tmp = getHead();
             while (tmp.data == value){
                 executeFirst();
                 tmp = getHead();
             }
+            //удаляем последние ноды, если они содержат указанное значение
             tmp = getTail();
             while(tmp.data == value){
                 executeLast();
                 tmp = getTail();
             }
+            /**
+             * первая и последняя ноды точно не содержат указанное значение и не подлежат удалению
+             */
             tmp = getHead();
+            //
             while (tmp.forvard != null){
-                if (tmp.forvard.data == value){
-                    //tmp.forvard = tmp.forvard.forvard;
+                //если данные текущей ноды не подлежат удалению, переходим к следующей ноде
+                if (tmp.data != value){
+                    tmp = tmp.forvard;
                 }
-                tmp = tmp.forvard;
-                
+                /**
+                 * если текущая нода подлежит удалению
+                 * (точно не первая и не последняя)
+                 * 
+                 */
+                else{
+                    tmp.forvard.backvard = tmp.backvard;
+                    tmp.backvard.forvard = tmp.forvard;
+                    tmp = tmp.forvard;
+                }
             }
         }
     }
-
+    /**
+     * добавление элементов массива в начало списка
+     * чтобы обеспечить последовательное размещение элементов массива в списке, элементы добавляются с последнего элемента массива
+     * @param array
+     */
+    public void arrayToHead(Integer[]array){
+        int i = array.length-1;
+        while (i >= 0){
+            addFirst(array[i]);
+            i--;
+        }
+    }
+    /**
+     * добавление элементов массива в хвост списка
+     * @param array
+     */
+    public void arrayToTail(Integer[]array){
+        int i = 0;
+        while (i < array.length){
+            addLast(array[i]);
+            i++;
+        }
+    }
+    /**
+     * добавление коллекции в начало списка
+     * @param collection
+     */
+    public void collectionToHead(ArrayList<Integer> collection){
+        for(int i = (collection.size()-1);i >= 0;i--) {
+            addFirst(collection.get(i));
+        }
+    }
+    /**
+     * добавление коллекции в хвост списка
+     * @param collection
+     */
+    public void collectionToTail(ArrayList<Integer> collection){
+        for(Integer c : collection){
+            addLast(c);
+        }
+    }
     public Node getHead() throws NullPointerException {
         return head;
     }
@@ -217,19 +287,19 @@ public class DoublyList {
 
         //выводим в консоль данные списка
         System.out.println("выводим в консоль данные списка ...");
-        list.printAll();
+        list.printForvard();
 
         //удаляем первый элемент списка
         System.out.println("удаляем первый элемент списка ..." + list.executeFirst());
         //выводим в консоль данные списка
         System.out.println("выводим в консоль данные списка ...");
-        list.printAll();
+        list.printForvard();
 
         //удаляем последний элемент списка
         System.out.println("удаляем последний элемент списка ..." + list.executeLast());
         //выводим в консоль данные списка
         System.out.println("выводим в консоль данные списка ...");
-        list.printAll();
+        list.printForvard();
 
         //проверяем наличие значения в списке
         int i = 23;
@@ -238,6 +308,55 @@ public class DoublyList {
         i = 0;
         System.out.println("Проверяем наличие значения в списке ... " + i);
         System.out.println("результат проверки: " + list.isContain(i));
+
+        //удаляем поле по заданному значению
+        i = 32;
+        System.out.println("удаляем элементы списка со значением ... " + i);
+        list.deleteByValue(i);
+        //выводим в консоль данные списка
+        System.out.println("выводим в консоль данные списка ...");
+        list.printForvard();
+
+        //массив с исходными данными
+        System.out.println("создаем новый массив arrays ...");
+        Integer[] array= {12, 22, 134,276,358,421,587,636,785,981};
+        System.out.println("содержащий данные: " + Arrays.toString(array));
+        System.out.println("добавляем массив в начало списка ...");
+        list.arrayToHead(array);
+        //выводим в консоль данные списка
+        System.out.println("выводим в консоль данные списка ...");
+        list.printForvard();
+        
+        //добавляем данные массива в конец списка
+        System.out.println("добавляем массив в конец списка ...");
+        list.arrayToTail(array);
+        //выводим в консоль данные списка
+        System.out.println("выводим в консоль данные списка ...");
+        list.printForvard();
+
+        //добавляем коллекцию в конец списка
+        System.out.println("добавляем коллекцию в конец списка ...");
+        ArrayList<Integer> collection = new ArrayList<Integer>();
+        collection.add(100);
+        collection.add(200);
+        collection.add(300);
+        collection.add(400);
+        collection.add(500);
+        collection.add(600);
+        list.collectionToTail(collection);
+        list.printForvard();
+
+        //добавляем коллекцию в начало списка
+        System.out.println("добавляем коллекцию в начало списка ...");
+        list.collectionToHead(collection);
+        list.printForvard();
+
+        //печатаем список в обратном порядке
+        System.out.println("печатаем список в обратном порядке ...");
+        list.printBackvard();
+        
+
+        
 
     }
 }
