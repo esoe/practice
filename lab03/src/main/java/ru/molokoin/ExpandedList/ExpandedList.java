66fea124126996df09b9d7eba4f067e5e185a3ab
esoe <igor_nikulin@mail.ru>
2022-10-30 12:40:31 +0300

    qq

--- lab03/src/main/java/ru/molokoin/ExpandedList/ExpandedList.java
+++ lab03/src/main/java/ru/molokoin/ExpandedList/ExpandedList.java
@@ -1,5 +1,8 @@
 package ru.molokoin.ExpandedList;
 
 public class ExpandedList {
+    public static void main(String[] args) {
+        System.out.println("run thread: " + Thread.currentThread().getName());
+    }
     
 }
