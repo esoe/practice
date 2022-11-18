6737a83336ffb8877743ff420f9b048b28214c37
esoe <igor_nikulin@mail.ru>
2022-11-18 21:18:26 +0300

    qq

--- lessons/src/main/java/ru/molokoin/j120/lab01/task5/Runner.java
+++ lessons/src/main/java/ru/molokoin/j120/lab01/task5/Runner.java
@@ -11,7 +11,18 @@ public class Runner {
         System.out.println("--------------------------------------");
         System.out.println("--------------------------------------");
 
-        
+        //исходные данные в массиве
+        Integer[] array = {9, 12, 24,38, 43, 51, 62, 79, 81, 99};
+        //создали новый список
+        DoublyGenericList<Integer> listInteger = new DoublyGenericList<Integer>();
+        //занесли данные массива в список
+        listInteger.arrayToHead(array);
+        //вывели данные списка в консоль
+        for (Integer integer : listInteger) {
+            
+        }
+
+
 
     }
 }
