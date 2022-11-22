44b23d4010628d4aae6f606aefe3eb05c37679ab
esoe <igor_nikulin@mail.ru>
2022-11-22 16:57:11 +0300

    qq

--- lessons/src/main/java/ru/molokoin/package00/App.java
+++ lessons/src/main/java/ru/molokoin/package00/App.java
@@ -1,9 +1,10 @@
 package ru.molokoin.package00;
 
+import java.awt.Desktop;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Запущен поток: " + Thread.currentThread().getName());
-        System.out.println("second");
-        System.out.println("rty");
+        System.out.println("qw");
+        Desktop.getDesktop().
     }
 }
