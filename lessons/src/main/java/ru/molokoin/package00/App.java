e489ea988effd9f8554a206119eb3b3c91218864
esoe <igor_nikulin@mail.ru>
2022-11-23 19:54:52 +0300

    qq

--- lessons/src/main/java/ru/molokoin/package00/App.java
+++ lessons/src/main/java/ru/molokoin/package00/App.java
@@ -1,10 +1,7 @@
 package ru.molokoin.package00;
 
-import java.awt.Desktop;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("qw");
-        
     }
 }
